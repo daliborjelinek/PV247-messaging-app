02c415fa0ca84e12541d4173598a1ed23eeb301b
tremors0 <janpavlu1@gmail.com>
2018-10-21 01:09:19 +0200

    Icons and styles for message editor.

--- src/components/MessageEditor.tsx
+++ src/components/MessageEditor.tsx
@@ -1,10 +1,57 @@
 import * as React from 'react';
+// Font awesome
+import {library as faIconLibrary} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBold, faEraser, faFile, faFileImage, faFont, faItalic, faLink, faListOl, faListUl, faSmile, faUnderline} from '@fortawesome/free-solid-svg-icons';
+
+faIconLibrary.add(faBold, faItalic, faUnderline, faFont, faEraser, faListOl,
+                  faListUl, faLink, faFile, faFileImage, faSmile);
 
 export class MessageEditor extends React.PureComponent {
+  private readonly messageEditorDiv: React.RefObject<HTMLDivElement>;
+
+  constructor(props: {}) {
+    super(props);
+    this.messageEditorDiv = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', () => this.resize());
+    this.resize();
+  }
+
+  private resize() {
+    const windowWidth = window.innerWidth;
+    const channelList = document.getElementsByClassName('ChannelList')[0] as HTMLDivElement;
+    if (channelList == null) {
+      return;
+    }
+    const channelListWidth = channelList.offsetWidth;
+    this.messageEditorDiv!.current!.style.width = windowWidth - channelListWidth + 'px';
+  }
 
   public render(): JSX.Element {
     return (
-      <div className={'MessageEditor'}> Here is going to be WYSIWYG editor.</div>
+      <div className={'MessageEditor'} ref={this.messageEditorDiv}>
+        <div className={'MessageEditor__operationPane'}>
+          <FontAwesomeIcon icon={'bold'} size={'lg'}/>
+          <FontAwesomeIcon icon={'italic'} size={'lg'}/>
+          <FontAwesomeIcon icon={'underline'} size={'lg'}/>
+          <span className={'glyphicon glyphicon-text-size'} />
+          <span className={'glyphicon glyphicon-text-color'} />
+          <FontAwesomeIcon icon={'eraser'} size={'lg'}/>
+          <FontAwesomeIcon icon={'list-ol'} size={'lg'} />
+          <FontAwesomeIcon icon={'list-ul'} size={'lg'}/>
+          <FontAwesomeIcon icon={'link'} size={'lg'}/>
+          <FontAwesomeIcon icon={'file-image'} size={'lg'}/>
+          <FontAwesomeIcon icon={'file'} size={'lg'}/>
+          <FontAwesomeIcon icon={'smile'} size={'lg'}/>
+        </div>
+        <div className={'MessageEditor__textAreaWrapper'}>
+          <textarea className={'MessageEditor__textArea'}/>
+          <button type={'submit'} className={'btn btn-primary'}>Send</button>
+        </div>
+      </div>
     );
   }
 }
