3b9482b73750bb85d282f7415d0feef3774e3f86
tremors0 <janpavlu1@gmail.com>
2019-01-02 19:54:54 +0100

    Types for requests

--- src/service/fileService.ts
+++ src/service/fileService.ts
@@ -5,13 +5,13 @@ import {ServerResponseFile, ServerResponseFileLink} from '../@types/api';
 export async function storeFileToServer(file: File): Promise<ServerResponseFile[]> {
   const formData = new FormData();
   formData.append('Files', file);
-  const response = await POST<any>(getFileUrl(), formData);
+  const response = await POST<ServerResponseFile[]>(getFileUrl(), formData);
   return response.data;
 }
 
 export async function getStoredFileURL(id: Uuid): Promise<ServerResponseFileLink> {
 
-  const response = await GET<any>(getFileLinkUrl(id));
+  const response = await GET<ServerResponseFileLink>(getFileLinkUrl(id));
   return response.data;
 }
 
