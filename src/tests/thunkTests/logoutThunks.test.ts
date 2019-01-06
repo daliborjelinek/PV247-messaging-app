6ca3bdd765a1c5a9e9eeb6cd76f0c7e19be29a91
tremors0 <janpavlu1@gmail.com>
2019-01-06 18:04:23 +0100

    Log out thunk action test

---
+++ src/tests/thunkTests/logoutThunks.test.ts
@@ -0,0 +1,24 @@
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import * as AuthenticationService from '../../service/authenticationService';
+import {MESSAGE_APP_LOG_OUT} from '../../constants/actionTypes';
+import {logout} from '../../actions/logoutAction';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+test('Log out', () => {
+  // @ts-ignore
+  AuthenticationService.logOut = jest.fn();
+
+  const store = mockStore({});
+
+  const expectedActions = [
+    { type: MESSAGE_APP_LOG_OUT },
+  ];
+
+  return store.dispatch(logout()).then(() => {
+    // return of async actions
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+});
